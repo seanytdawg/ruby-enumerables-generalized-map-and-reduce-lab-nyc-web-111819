2136b3f9618449fdfbd0120d06671522bc760761
Sean Tarzy <seantarzy@gmail.com>
2019-11-17 04:11:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
-# Your Code Here
+def map_1(source_array)
+  new = []
+  i = 0 
+   while i < source_array.length 
+        new.push((source_array[i]) * 2)
+  i += 1
+end 
+return new
+end
+ 
+ def map_2(source_array)
+  new = []
+  i = 0 
+   while i < source_array.length 
+        new.push((source_array[i]) * -1)
+  i += 1
+end 
+return new
+end
+
+ def map_to_no_change(source_array)
+ new = []
+  i = 0 
+   while i < source_array.length 
+        new.push((source_array[i]) * 1)
+  i += 1
+end 
+return new
+end
+ 
+def map_to_square(source_array)
+  source_array.map do |num|
+    num ** 2 
+  end
+end
+
+# def my_own_reduce()
