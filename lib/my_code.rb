6ff63b9e774c66820d0b1b92bef06b063de28bcd
Sean Tarzy <seantarzy@gmail.com>
2019-11-17 04:18:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,39 +1,39 @@
-# def map(source_array)
-#   new = []
-#   i = 0 
-#   while i < source_array.length 
-#         new.push((source_array[i]) * 2)
-#   i += 1
-# end 
-# return new
-# end
- 
 def map(source_array)
   new = []
   i = 0 
   while i < source_array.length 
-        new.push((source_array[i]) * -1)
+        new.push(yield(source_array[i]) * 2)
   i += 1
 end 
 return new
 end
-
+ 
 # def map(source_array)
-# new = []
+#   new = []
 #   i = 0 
 #   while i < source_array.length 
-#         new.push((source_array[i]) * 1)
+#         new.push((source_array[i]) * -1)
 #   i += 1
 # end 
 # return new
 # end
- 
-# def map(source_array)
-#   new_array = []
-#   source_array.each do |num|
-#   ele = num ** 2 
-#   end 
-#   new_array << ele 
-# end
 
-# def my_own_reduce()
+# # def map(source_array)
+# # new = []
+# #   i = 0 
+# #   while i < source_array.length 
+# #         new.push((source_array[i]) * 1)
+# #   i += 1
+# # end 
+# # return new
+# # end
+ 
+# # def map(source_array)
+# #   new_array = []
+# #   source_array.each do |num|
+# #   ele = num ** 2 
+# #   end 
+# #   new_array << ele 
+# # end
+
+# # def my_own_reduce()
