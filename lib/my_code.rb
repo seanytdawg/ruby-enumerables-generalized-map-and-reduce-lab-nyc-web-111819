62877d99d189d848dfbd6cfbcc9c7d81b464a5a2
Sean Tarzy <seantarzy@gmail.com>
2019-11-17 04:17:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,32 +8,32 @@
 # return new
 # end
  
-# def map(source_array)
-#   new = []
-#   i = 0 
-#   while i < source_array.length 
-#         new.push((source_array[i]) * -1)
-#   i += 1
-# end 
-# return new
-# end
-
 def map(source_array)
-new = []
+  new = []
   i = 0 
   while i < source_array.length 
-        new.push((source_array[i]) * 1)
+        new.push((source_array[i]) * -1)
   i += 1
 end 
 return new
 end
- 
-def map(source_array)
-  new_array = []
-  source_array.each do |num|
-   ele = num ** 2 
-  end 
-  new_array << ele 
-end
 
-def my_own_reduce()
+# def map(source_array)
+# new = []
+#   i = 0 
+#   while i < source_array.length 
+#         new.push((source_array[i]) * 1)
+#   i += 1
+# end 
+# return new
+# end
+ 
+# def map(source_array)
+#   new_array = []
+#   source_array.each do |num|
+#   ele = num ** 2 
+#   end 
+#   new_array << ele 
+# end
+
+# def my_own_reduce()
